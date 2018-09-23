327aca2412aa454054308ef466f48ec2056b24b2
Chris Allen <chrishughallen@gmail.com>
2018-09-23 10:59:09 -0500

    initial commit of age guessing application

---
+++ src/main/java/com/chrisallen/guessmyage/GuessmyageApplication.java
@@ -0,0 +1,12 @@
+package com.chrisallen.guessmyage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuessmyageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuessmyageApplication.class, args);
+	}
+}
---
+++ src/test/java/com/chrisallen/guessmyage/GuessmyageApplicationTests.java
@@ -0,0 +1,16 @@
+package com.chrisallen.guessmyage;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GuessmyageApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
